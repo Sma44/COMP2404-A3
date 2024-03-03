33064d5a3b2026d480141411f0b51ffc82f95fa4
Sma44 <samsonma44@gmail.com>
2024-03-03 17:10:43 -0500

    implemented Course class

--- Course.cc
+++ Course.cc
@@ -2,3 +2,40 @@
 #include <iomanip>
 #include <iostream>
 #include <string>
+#include "defs.h"
+using namespace std;
+
+int Course::nextId = COURSE_ID;
+
+Course::Course(string t, string s, int c, char sec, string i) 
+: id(nextId++), term(t), subject(s), code(c), section(sec), instructor(i){}
+
+bool Course::lessThan(Course* course) const {
+
+    if (term < course->term){ return true; }
+    if (term > course->term){ return false; }
+
+    if (subject < course->subject){ return true; }
+    if (subject > course->subject){ return false; }
+
+    if (code < course->code){ return true; }
+    if (code > course->code){ return false; }
+
+    if (section < course->section){ return true; }
+    if (section > course->section){ return false; }
+
+    return false;
+}
+
+void Course::print() const {
+    cout << setw(6) << left << id << "Term:" << term << setw(5) << subject
+    << code << setw(4) << section << setw(7) << "Instr:" << setw(10) << instructor;
+}
+
+string Course::getTerm() const { return term; }
+
+int Course::getId() const { return id; }
+
+string Course::getFormatString() const {
+    return subject + " " + to_string(code) + "-" + section;
+}
--- Course.h
+++ Course.h
@@ -1,10 +1,25 @@
 #ifndef COURSE_H
 #define COURSE_H
+#include <string>
+#include "defs.h"
 
-class course{
+class Course{
     public:
+        Course(std::string="N/A", std::string="N/A", int=0, char='N', std::string="N/A");
+        bool lessThan(Course*) const;
+        void print() const;
+        int getId() const;
+        std::string getTerm() const;
+        std::string getFormatString() const;
 
     private:
+        static int nextId; 
+        int id;
+        std::string term;
+        std::string subject;
+        int code;
+        char section;
+        std::string instructor;
 
 };
 
