8b86acdd489dbb2ca07301d2683cd2ab49b37495
Sma44 <samsonma44@gmail.com>
2024-03-04 23:04:26 -0500

    school class implemented

---
+++ School.cc
@@ -0,0 +1,58 @@
+#include "School.h"
+#include <string>
+#include <iomanip>
+#include <iostream>
+using namespace std;
+
+School::School(string n): name(n){}
+
+School::~School(){
+    regList.cleanData();
+}
+
+void School::addStudent(Student* stu){
+    stuArr.add(stu);
+}
+
+void School::AddCourse(Course* course){
+    courseArr.add(course);
+}
+
+void School::addRegistration(Student* stu, Course* course){
+    Registration* newReg = new Registration(stu, course);
+    regList.add(newReg);
+}
+
+bool School::findStudent(string num, Student** stu){
+    return stuArr.find(num, stu);
+}
+
+bool School::findCourse(int id, Course** course){
+    return courseArr.find(id, course);
+}
+
+void School::printStudents(){
+    cout << "STUDENTS OF " << name << endl;
+    stuArr.print();
+}
+
+void School::printCourses(){
+    cout << "COURSES OF " << name << endl;
+    courseArr.print();
+}
+
+void School::printCourseByTerm(string term){
+    cout << "COURSES OF " << name << " - TERM: " << term << endl;
+    courseArr.print(term);
+}
+
+void School::printRegistrations(){
+    cout << "REGISTRATIONS OF " << name << endl;
+    regList.print();
+}
+
+void School::printRegistrationsByStu(Student* stu){
+    cout << "REGISTRATIONS OF " << name << " - FOR STUDENT: " << stu->getName() << endl;
+    RegList stuList(regList, stu);
+    stuList.print();
+}
---
+++ School.h
@@ -0,0 +1,30 @@
+#ifndef SCHOOL_H
+#define SCHOOL_H
+#include "StuArray.h"
+#include "CourseArray.h"
+#include "RegList.h"
+#include <string>
+
+class School{
+    public:
+        School(std::string="Not Defined");
+        ~School();
+        void addStudent(Student*);
+        void AddCourse(Course*);
+        void addRegistration(Student*, Course*);
+        bool findStudent(std::string, Student**);
+        bool findCourse(int, Course**);
+        void printStudents();
+        void printCourses();
+        void printCourseByTerm(std::string);
+        void printRegistrations();
+        void printRegistrationsByStu(Student*);
+
+    private:
+        std::string name;
+        StuArray stuArr;
+        CourseArray courseArr;
+        RegList regList;
+};
+
+#endif
